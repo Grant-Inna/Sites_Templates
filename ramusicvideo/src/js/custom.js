d1dd0aa5fb1f13b0bc014ce7c9a3f6fd4dde9a53
Grant-Inna <grantinna@mail.ru>
2018-06-05 17:18:08 +0300

    New page with modal window and new state "log in"

--- ramusicvideo/src/js/custom.js
+++ ramusicvideo/src/js/custom.js
@@ -3,14 +3,14 @@ $(function(){
     $( "#mMenu" ).click( function() {
         $( "#mMenu" ).toggleClass( "menu-open" );
         $( ".menu-block__wrapper" ).toggleClass( "menu-show" )
-    })
+    });
     $( ".section__container" ).bind( "mouseenter mouseleave", function() {
         $( '.section__popUp-block', this ).toggleClass("show-popUp");
         
         setTimeout(
             $( '.section__popUp-block', this ).toggleClass("display-popUp"), 4000);
-    
-        //
-        // console.info( 'Пиу!' )
     });
+    $( ".notice__cross, .notice__button" ).click( function (){
+        $( '.notice__block-back' ).hide();
+    } )
 })(jQuery);
